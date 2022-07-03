727454072fffc1de6923c5c19b808cac3c7487ad
johnny2smooth <johnbp13@gmail.com>
2022-07-03 13:44:11 -0700

    introduction bones layed out

--- components/introduction.js
+++ components/introduction.js
@@ -1,10 +1,24 @@
+import Link from 'next/link';
+
 export default function Introduction() {
   return (
-    <>
-      <h1 className="my-6 text-4xl">
+    <div className="flex flex-wrap ">
+      <h1 className=" text-4xl w-full mx-auto max-w-lg my-6">
         Hi, <strong className="text-green-600"> I'm Johnny</strong>. I want to
         help build speedy & elegant products with you!
       </h1>
-    </>
+      <p className="max-w-prose my-6">
+        I am currently working as a full-stack developer for a{' '}
+        <Link href="/">
+          <a>research team at the University of Washington</a>
+        </Link>
+        . I spend my free time building{' '}
+        <Link href="/">
+          <a>various projects</a>
+        </Link>{' '}
+        to show off on here, playing tennis & basketball, and sharing laughs
+        with my friends.
+      </p>
+    </div>
   );
 }
