6f0dc639c2941e302a594aa0aa1aa9ce4d69ac50
johnny2smooth <johnbp13@gmail.com>
2022-07-05 10:54:00 -0700

    error fixed in highlight section + blog post added

--- components/highlights.js
+++ components/highlights.js
@@ -16,41 +16,41 @@ export default function Highlights() {
         <Link href="/">
           <motion.a
             href="/"
-            class=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
+            className=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
           >
-            <p class="mx-auto my-auto text-lg font-black">1</p>
+            <p className="mx-auto my-auto text-lg font-black">1</p>
           </motion.a>
         </Link>
         <Link href="/">
           <motion.a
             href="/"
-            class=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
+            className=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
           >
-            <p class="mx-auto my-auto text-lg font-black">2</p>
+            <p className="mx-auto my-auto text-lg font-black">2</p>
           </motion.a>
         </Link>
         <Link href="/">
           <motion.a
             href="/"
-            class=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
+            className=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
           >
-            <p class="mx-auto my-auto text-lg font-black">3</p>
+            <p className="mx-auto my-auto text-lg font-black">3</p>
           </motion.a>
         </Link>
         <Link href="/">
           <motion.a
             href="/"
-            class=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
+            className=" bg-whiteLight mx-auto w-full rounded h-32 flex border-4 border-blueWater shadow-lg"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
           >
-            <p class="mx-auto my-auto text-lg font-black">4</p>
+            <p className="mx-auto my-auto text-lg font-black">4</p>
           </motion.a>
         </Link>
       </motion.div>
